458dfee4b4b94db5a42892dc45fb119605916266
matias <github.com.wifi954@passmail.net>
2025-07-13 23:22:27 -0300

    ejercicio1-3

---
+++ ejercicio1-3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+
+    // Entrada de datos
+    cout << "Ingrese el valor de A: ";
+    cin >> A;
+    cout << "Ingrese el valor de B: ";
+    cin >> B;
+
+    // Operaciones
+    int suma = A + B;
+    int resta = A - B;
+    int producto = A * B;
+
+    // Resultados
+    cout << "La suma de A y B es: " << suma << endl;
+    cout << "La resta de A y B es: " << resta << endl;
+    cout << "El producto de A y B es: " << producto << endl;
+
+    return 0;
+}
